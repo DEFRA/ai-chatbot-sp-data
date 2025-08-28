a6b24bbaa5b7b6b76093cdd966f95bbd6acda1c3
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2025-08-28 15:45:03 +0100

    Remove example endpoints

--- app/entrypoints/fastapi.py
+++ app/entrypoints/fastapi.py
@@ -7,7 +7,6 @@ from fastapi import FastAPI
 from app.common.mongo import get_mongo_client
 from app.common.tracing import TraceIdMiddleware
 from app.config import config
-from app.example.router import router as example_router
 from app.health.router import router as health_router
 
 logger = getLogger(__name__)
@@ -32,7 +31,6 @@ app.add_middleware(TraceIdMiddleware)
 
 # Setup Routes
 app.include_router(health_router)
-app.include_router(example_router)
 
 
 def main() -> None:
