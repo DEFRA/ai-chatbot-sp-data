26ba870d5cccf75acf37a178c26d1940c1ece7d0
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2025-08-28 15:43:59 +0100

    Flatten entrypoints

--- app/entrypoints/fastapi.py
+++ app/entrypoints/fastapi.py
@@ -37,7 +37,7 @@ app.include_router(example_router)
 
 def main() -> None:
     uvicorn.run(
-        "app.entrypoints.http.main:app",
+        "app.entrypoints.fastapi:app",
         host=config.host,
         port=config.port,
         log_config=config.log_config,
